149afe06cf81108f62f32477541cae478d00a103
yunheeAhn <ayh95944674@gmail.com>
2023-12-19 14:36:47 +0900

    scss setting 231219

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # --html-template-scss
 Default SCSS template settings
+
+2023-12-19
+: Start default scss(sass) template setting!
---
+++ assets/js/comm.js
@@ -0,0 +1,7 @@
+js;
+// 스와이퍼 js 샘플
+new Swiper(".swiper-container", {
+    direction: "vertical", // 수직 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true, // 반복 재생 여부
+  });
